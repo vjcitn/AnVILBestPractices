7c9b829b47e4fc42c6a4c9085c1245139bd4aecb
vjcitn <stvjc@channing.harvard.edu>
2024-03-17 16:04:44 -0400

    trying to pass BiocCheck

--- R/foo.R
+++ R/foo.R
@@ -1,3 +1,15 @@
+#' Get a Single Cell Experiment
+#' @importFrom celldex DatabaseImmuneCellExpressionData
+#' @import SingleCellExperiment
+#' @examples
+#' di = get_dice()
+#' table(di$label.main)
+#' @export
+get_dice = function()
+  celldex::DatabaseImmuneCellExpressionData()
+
 #' placeholder
+#' @examples
+#' foo()
 #' @export
 foo = function() 1
